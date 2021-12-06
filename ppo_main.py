6cab72997e56c9785d51ae63bdd0239d2fcefc7d
shoang22 <shoang@masonlive.gmu.edu>
2021-12-06 13:25:35 +0400

    added ppo

--- ppo_main.py
+++ ppo_main.py
@@ -1,25 +1,24 @@
 import gym
 import numpy as np
-from ppo_torch_cont import Agent
-# from ppo_torch import Agent
+from ppo_torch_discrete import Agent
 import time
 
 if __name__ == '__main__':
-    # initialize environment and initial conditions
     start_time = time.time()
-    env_name = 'MountainCarContinuous-v0'
-    env = gym.make(env_name)
-    N = 20  # trajectories to store in memory
-    batch_size = 5  # batchsize for update
-    n_epochs = 4
-    alpha = 0.001
 
-    # initialize agent
-    agent = Agent(env=env, batch_size=batch_size,
-                    alpha=alpha, n_epochs=n_epochs)
+    # initialize environment and initial conditions
+    env_name = 'LunarLander-v2'
+    env = gym.make(env_name)
+    N = 20
+    batch_size = 5
+    n_epochs = 4
+    alpha = 0.0001
+    agent = Agent(n_actions=env.action_space.n, batch_size=batch_size,
+                    alpha=alpha, n_epochs=n_epochs,
+                    input_dims=env.observation_space.shape)
     n_games = 10000
 
-    figure_file = f'plots/{env_name}_cont.png'
+    figure_file = f'plots/{env_name}.png'
 
     best_score = env.reward_range[0]
     score_history = []
@@ -35,13 +34,12 @@ if __name__ == '__main__':
         while not done:
             env.render()
             action, prob, val = agent.choose_action(observation)
-            observation_, reward, done, info = env.step([action])  # need to make into array to convert to tensor
+            observation_, reward, done, info = env.step(action)
             n_steps += 1
 
-            score += reward
-            agent.remember(observation, action, val, prob, reward, done)  # store trajectory in PPO memory
 
-            # update parameters when memory is full
+            score += reward
+            agent.remember(observation, action, prob, val, reward, done)
             if n_steps % N == 0:
                 agent.learn()
                 learn_iters += 1
@@ -56,14 +54,16 @@ if __name__ == '__main__':
         print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
                 'time_steps', n_steps, 'learning_steps', learn_iters)
 
+    print("--- %s seconds to train---" % (time.time() - start_time))
 
     agent.save_agent(actor_path=env_name + '_PPO_actor.h5',critic_path=env_name + '_PPO_critic.h5')
-    np.save('npy/'+env_name+'_PPO.npy',score_history)
+    np.save(env_name+'_PPO.npy',score_history)
     x = [i+1 for i in range(len(score_history))]
     env.close()
-    
+
     end_time = time.time() - start_time
     time_text = f'the time it takes to run is {end_time}'
     print('time recorded')
     with open('times/'+env_name+'time.txt', 'w') as f:
         f.write(time_text)
+
